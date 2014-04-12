74c599aeb10c27eb26ea16837540a9ee329517fd
Jason Fieldman <jason@fieldman.org>
2014-04-12 13:58:20 -0700

    added settings manager skeleton

---
+++ Impression/Classes/Models/SettingsManager.h
@@ -0,0 +1,15 @@
+//
+//  SettingsManager.h
+//  Impression
+//
+//  Created by Jason Fieldman on 4/12/14.
+//  Copyright (c) 2014 Jason Fieldman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SettingsManager : NSObject
+
+SINGLETON_INTR(SettingsManager);
+
+@end
---
+++ Impression/Classes/Models/SettingsManager.m
@@ -0,0 +1,15 @@
+//
+//  SettingsManager.m
+//  Impression
+//
+//  Created by Jason Fieldman on 4/12/14.
+//  Copyright (c) 2014 Jason Fieldman. All rights reserved.
+//
+
+#import "SettingsManager.h"
+
+@implementation SettingsManager
+
+SINGLETON_IMPL(SettingsManager);
+
+@end
