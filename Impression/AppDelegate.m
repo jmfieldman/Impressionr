c454ed3be702d4e343fac46efaddb1035c53d650
Jason Fieldman <jason@fieldman.org>
2014-04-15 12:34:29 -0700

    added flurry

--- Impression/AppDelegate.m
+++ Impression/AppDelegate.m
@@ -19,6 +19,9 @@
 	/* Initialize sfx */
 	[PreloadedSFX initializePreloadedSFX];
 	
+	/* Flurry */
+	[Flurry startSession:@"B775NFHYNN3Z3687DYMN"];
+	
 	self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 	// Override point for customization after application launch.
 	self.window.backgroundColor = [UIColor whiteColor];
